f9870cde7e495282b59e4a0bbb8899d802cb6bbb
Daipn <daipn1@fsoft.com.vn>
2025-06-27 17:39:57 +0800

    Sherlock String Problem

---
+++ problems/sherlock_string/problem.md
@@ -0,0 +1,1 @@
+https://www.hackerrank.com/challenges/three-month-preparation-kit-sherlock-and-valid-string/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-seven
---
+++ tests/sherlock_string/test_sherlock_string.py
@@ -0,0 +1,26 @@
+import pytest
+from problems.sherlock_string.sherlock_string import sherlock_string
+
+
+def test_can_call_sherlock_string():
+    sherlock_string("aaabbbccc")
+
+
+def test_sherlock_string_with_all_same_characters():
+    assert sherlock_string("aaaaaa") == "YES"
+
+
+def test_sherlock_string_with_2_different_chars_and_same_frequency_return_yes():
+    assert sherlock_string("aabb") == "YES"
+
+
+def test_sherlock_string_with_3_different_frequencies_return_no():
+    assert sherlock_string("aaabbccdde") == "NO"
+
+
+def test_sherlock_string_with_2_different_chars_and_frequency_difference_return_no():
+    assert sherlock_string("aabbbb") == "NO"
+
+
+def test_sherlock_string_with_one_char_different_frequency_return_yes():
+    assert sherlock_string("aabbccdde") == "YES"
