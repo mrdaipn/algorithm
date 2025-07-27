b5fa95c04e9fd0be471b9f9d80ef8f22a23c9353
Daipn <daipn1@fsoft.com.vn>
2025-07-27 21:21:01 +0800

    Min window to sort an array - optimized version

--- problems/minimum_window_to_sort_array/minimum_window_to_sort_arry.py
+++ problems/minimum_window_to_sort_array/minimum_window_to_sort_arry.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -19,3 +20,34 @@ def minimum_window_to_sort(arr: List[int]) -> tuple[int, int]:
             break
 
     return start, end
+
+
+def minimum_window_to_sort_optimized(arr: List[str]):
+    """Return the left, right index of the arr which need to rearrange to have shorted list."""
+    left = __find_left(arr)
+    if left >= len(arr):
+        return 0, 0
+    right = __find_right(arr, arr[left])
+    return (left, right) if left < right else (0, len(arr) - 1)
+
+
+def __find_left(arr):
+    current_index = 0
+    max_so_far = -math.inf
+    while True:
+        if arr[current_index] < max_so_far:
+            return current_index - 1
+
+        max_so_far = arr[current_index]
+        current_index += 1
+
+        if current_index == len(arr):
+            return current_index
+
+
+def __find_right(arr, left_value):
+    current_index = len(arr) - 1
+    while current_index > -1 and arr[current_index] > left_value:
+        current_index -= 1
+
+    return current_index
--- tests/minimum_window_to_sort_array/test_minimum_window_to_sort_array.py
+++ tests/minimum_window_to_sort_array/test_minimum_window_to_sort_array.py
@@ -1,9 +1,10 @@
 from problems.minimum_window_to_sort_array.minimum_window_to_sort_arry import (
     minimum_window_to_sort,
+    minimum_window_to_sort_optimized,
 )
 
 
-def test_able_to_call_minimum_window_to_sort_string_function():
+def test_able_to_call_minimum_window_to_sort_with_random_array():
     assert minimum_window_to_sort([3, 7, 5, 6, 9]) == (1, 3)
 
 
@@ -13,3 +14,15 @@ def test_minimum_window_to_sort_with_decreasing_array():
 
 def test_minimum_window_to_sort_with_sorted_array():
     assert minimum_window_to_sort([1, 2, 3, 4, 5]) == (0, 0)
+
+
+def test_able_to_call_minimum_window_to_sort_optimized_with_random_array():
+    assert minimum_window_to_sort_optimized([3, 7, 5, 6, 9]) == (1, 3)
+
+
+def test_minimum_window_to_sort_optimized_with_decreasing_array():
+    assert minimum_window_to_sort_optimized([5, 4, 3, 2, 1]) == (0, 4)
+
+
+def test_minimum_window_to_sort_optimized_with_sorted_array():
+    assert minimum_window_to_sort_optimized([1, 2, 3, 4, 5]) == (0, 0)
